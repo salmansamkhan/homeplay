1d186950d23fa63f88085b4732fd87a51f4afa68
Pratik <pratik@itccdigital.com>
2020-10-01 23:22:08 +0530

    artist-2

---
+++ artist-2.php
@@ -0,0 +1,275 @@
+<?php include("header.php");?>
+
+	<!--Web_Main_BG-->
+    <div class="web_main_bg">
+		<div class="container">
+			<div class="main_bg">
+				<div class="row align-items-center">
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						<div class="hp_main_text">
+							<h2>Book Your Show Now<span class="blink_homeplay">.</span></h2>
+							<h3>Put Yourself Under The <span class="blink_homeplay">SpotLight</span></h3>
+							<p>Looking to boost your viewership, listeners, followers, and more? Look no further than the HomePlay platform to prop you up and put you infront of thousands of paying viewers!</p>
+							<div class="hp_main_buttons">
+								<a href="#"><img src="images/upcoming_events_icon.png" class="img-fluid">Upcoming Events</a>
+								<a href="#"><img src="images/featured_event_icon.png" class="img-fluid">Featured Events</a>
+								<a href="#"><img src="images/all_events_icon.png" class="img-fluid">All Events</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+	<!--Web_Main_BG-->
+	
+	<!--Web_Main_Box-->
+	<div class="web_main_box">
+        <div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="hp_main_bottom">
+						<a href="#featured_events"><img src="images/main_scroll.png" class="img-fluid"></a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="main_box">
+						<img src="images/Microphone-Icon.png" alt=""/>
+						<h3>Professional Artists</h3>
+						<p>HomePlay believes in providing our artists a platform to show off their talents and grow their reach without getting buried in technical responsibilities.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="main_box">
+						<img src="images/High-Def-Video-Icon.png" alt=""/>
+						<h3>High-Definition Video</h3>
+						<p>HomePlay has a strong relationship with the international high-quality webcasting/streaming experts at iSiLIVE. HD audio and video are a requirement that we will not compromise</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="main_box">
+						<img src="images/Payment-Icon.png" alt=""/>
+						<h3>Secure Payment</h3>
+						<p >HomePlay uses Stripes payment platform which is completely secure and means that none of your data or personal information passes through our servers.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div >
+	<!--Web_Main_Box-->
+	
+	<div class="spacer" id="featured_events"></div>
+	
+	<!--Web_Our_Process-->
+	<div class="web_our_process">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="common_title">
+						<h2>Our Process</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/> tempor incididunt ut labore et dolore magna aliqua.</p>
+						<span class="ct_line"></span>
+					</div>
+				</div>
+			</div>
+			<div class="our_process">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">1</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Register an Event</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">2</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Set a Date</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">3</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Preparing the Event</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">4</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Start Promoting</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">5</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Go Live on HomePLay</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<div class="our_process_tab">
+							<span class="number">6</span>
+							<div class="our_pt_img">
+								<img src="images/our_process_icon1.png" alt=""/>
+							</div>
+							<div class="our_pt_text">
+								<h4>Your Event Archive</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Web_Our_Process-->
+	
+	<!--Web_Featured_Events-->
+	<div class="web_featured_events">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="common_title">
+						<h2>UPCOMING EVENTS</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/> tempor incididunt ut labore et dolore magna aliqua.</p>
+						<span class="ct_line"></span>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+					<div class="event_common_tab">
+						<img src="images/event_img4.jpg" alt=""/>
+						<div class="event_ct_date">
+							<span class="ct_one">DEC</span>
+							<span class="ct_two">30-2020</span>
+						</div>
+						<h4>Event Title Goes Here</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+						<a href="#" class="event_ct_btn">Pop / R&amp;B</a>
+						<div class="event_ct_bottom">
+							<label><a href="#">Get Ticket</a></label>
+							<span class="ct_price">$89.99</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+					<div class="event_common_tab">
+						<img src="images/event_img5.jpg" alt=""/>
+						<div class="event_ct_date">
+							<span class="ct_one">DEC</span>
+							<span class="ct_two">30-2020</span>
+						</div>
+						<h4>Event Title Goes Here</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+						<a href="#" class="event_ct_btn">Pop / R&amp;B</a>
+						<div class="event_ct_bottom">
+							<label><a href="#">Get Ticket</a></label>
+							<span class="ct_price">$89.99</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+					<div class="event_common_tab">
+						<img src="images/event_img6.jpg" alt=""/>
+						<div class="event_ct_date">
+							<span class="ct_one">DEC</span>
+							<span class="ct_two">30-2020</span>
+						</div>
+						<h4>Event Title Goes Here</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+						<a href="#" class="event_ct_btn">Pop / R&amp;B</a>
+						<div class="event_ct_bottom">
+							<label><a href="#">Get Ticket</a></label>
+							<span class="ct_price">$89.99</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+					<div class="event_common_tab">
+						<img src="images/event_img7.jpg" alt=""/>
+						<div class="event_ct_date">
+							<span class="ct_one">DEC</span>
+							<span class="ct_two">30-2020</span>
+						</div>
+						<h4>Event Title Goes Here</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+						<a href="#" class="event_ct_btn">Pop / R&amp;B</a>
+						<div class="event_ct_bottom">
+							<label><a href="#">Get Ticket</a></label>
+							<span class="ct_price">$89.99</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+					<div class="event_button">
+						<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>All Events</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Web_Featured_Events-->
+	
+	<!--Web_Video_Section-->
+	<div class="web_video_section">
+        <div class="container">
+			<div class="video_section_bg">
+				<div class="row">
+					<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+						<div class="video_section_text">
+							<h3>Join thousands of people</h3>
+							<p>HomePlay.LIVE is building a strong viewership base where you have the ability to get your performance, your music, your show in front of thousands of people at once. Professionally presenting artists is an incredibly important task. Designed for your big screen at home, our focus is on the highest level of production quality for our artists, and viewers.</p>
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+						<div class="video_section_button">
+							<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>Start Watching</span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--Web_Video_Section-->
+	
+<?php include("footer.php");?>
