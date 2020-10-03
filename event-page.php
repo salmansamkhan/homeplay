f17e9f0cc0214aa3c2490079b3c413dcb3be2777
Pratik <pratik@itccdigital.com>
2020-10-03 11:46:24 +0530

    responsive issue fix

---
+++ event-page.php
@@ -0,0 +1,253 @@
+<?php include("header.php");?>
+
+<!--Web_Artist_Landing_BG-->
+<div class="web_artist_landing_bg"></div>
+
+<div class="container">
+	<div class="artist_landing_bg">
+		<div class="row">
+			<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+				<div class="hp_artist_landing_profile">
+					<img src="images/profile_edit.jpg" alt=""/>
+					<a href="#" class="hp_artist_profile_plus"><span>Artist Profile <i class="fas fa-plus"></i></span><img src="images/artist_profile.png" alt=""/></a>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+				<div class="hp_artist_landing_info">
+					<a href="#" class="alp_tag">Country / Pop</a>
+					<h2>Kathleen Edwards <span class="blink_homeplay">.</span> <a href="#" class="alp_edit">Edit</a></h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					<div class="hp_al_date_time">
+						<i class="fa fa-calendar"></i>
+						<span>01/02/2020</span>
+						<span>1:00PM</span>
+					</div>
+					<div class="hp_al_get_tickets">
+						<h3>Price: <span class="gl_price">$109.99</span></h3>
+						<a href="#">Get Tickets</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+				<div class="hp_al_bio">
+					<h3>Biography</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+				<div class="hp_al_upcoming_event">
+					<h3>Upcoming Events</h3>
+					<div class="al_upcoming_events_tabs">
+						<div class="al_upcoming_events_tab">
+							<div class="row">
+								<div class="col-lg-2 col-md-2 col-sm-2">
+									<img src="images/blank_thum.jpg" alt=""/>
+								</div>
+								<div class="col-lg-6 col-md-6 col-sm-6">
+									<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+									<p>01/02/2020</p>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<div class="al_ue_button">
+										<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>Get Tickets</span></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="al_upcoming_events_tab">
+							<div class="row">
+								<div class="col-lg-2 col-md-2 col-sm-2">
+									<img src="images/blank_thum.jpg" alt=""/>
+								</div>
+								<div class="col-lg-6 col-md-6 col-sm-6">
+									<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+									<p>01/02/2020</p>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<div class="al_ue_button">
+										<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>Get Tickets</span></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="al_upcoming_events_tab">
+							<div class="row">
+								<div class="col-lg-2 col-md-2 col-sm-2">
+									<img src="images/blank_thum.jpg" alt=""/>
+								</div>
+								<div class="col-lg-6 col-md-6 col-sm-6">
+									<h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+									<p>01/02/2020</p>
+								</div>
+								<div class="col-lg-4 col-md-4 col-sm-4">
+									<div class="al_ue_button">
+										<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>Get Tickets</span></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Web_Artist_Landing_BG-->
+
+<!--Web_Featured_Events-->
+<div class="web_featured_events" >
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="common_title">
+					<h2>EVENTS ARCHIVE</h2>
+					<span class="ct_line"></span>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/> tempor incididunt ut labore et dolore magna aliqua.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="owl-carousel owl-theme event_video_tabs">
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<a href="#" class="evt_tag">New</a>
+								<img src="images/event_video1.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video2.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video3.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<a href="#" class="evt_tag">New</a>
+								<img src="images/event_video1.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video2.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video3.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<a href="#" class="evt_tag">New</a>
+								<img src="images/event_video1.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video2.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+					<div class="item">
+						<div class="event_video_tab">
+							<div class="evt_video">
+								<img src="images/event_video3.jpg" alt=""/>
+							</div>
+							<div class="evt_text">
+								<h4>Event Title Goes Here</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+								<span class="evt_date">12/30/2020</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Web_Featured_Events-->
+
+<!--Web_Video_Section-->
+<div class="web_video_section">
+	<div class="container">
+		<div class="video_section_bg">
+			<div class="row">
+				<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+					<div class="video_section_text">
+						<h3>Join thousands of people</h3>
+						<p>HomePlay.LIVE is building a strong viewership base where you have the ability to get your performance, your music, your show in front of thousands of people at once. Professionally presenting artists is an incredibly important task. Designed for your big screen at home, our focus is on the highest level of production quality for our artists, and viewers.</p>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 text-center">
+					<div class="video_section_button">
+						<a href="#"><img src="images/arrow_icon.png" alt=""/> <span>Start Watching</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--Web_Video_Section-->
+<?php include("footer.php");?>
